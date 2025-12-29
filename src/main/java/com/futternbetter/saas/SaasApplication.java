e92b22eb245798abfde3f1f4b9e059975a4ec6a4
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2025-12-29 19:26:18 +0700

    init commit

---
+++ src/main/java/com/futternbetter/saas/SaasApplication.java
@@ -0,0 +1,15 @@
+package com.futternbetter.saas;
+
+import com.futternbetter.saas.config.DotEnvConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaasApplication {
+
+    public static void main(String[] args) {
+        DotEnvConfig.loadEnv();
+        SpringApplication.run(SaasApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/futternbetter/saas/config/DotEnvConfig.java
@@ -0,0 +1,15 @@
+package com.futternbetter.saas.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class DotEnvConfig {
+
+    public static void loadEnv() {
+        Dotenv dotenv = Dotenv.configure().filename(".env").load(); // sẽ load từ classpath
+        dotenv
+                .entries()
+                .forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
+}
