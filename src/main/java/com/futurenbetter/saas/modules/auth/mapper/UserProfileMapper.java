aa153950e8c86d3e257603863cab2d3259f160e0
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2026-02-04 11:25:47 +0700

    mapper

--- src/main/java/com/futurenbetter/saas/modules/auth/mapper/UserProfileMapper.java
+++ src/main/java/com/futurenbetter/saas/modules/auth/mapper/UserProfileMapper.java
@@ -20,6 +20,14 @@ public interface UserProfileMapper {
     SystemAdminRegistrationResponse toAdminResponse(UserProfile userProfile);
 
     @Mapping(target = "employee", source = "employeeResponse")
+    @Mapping(target = "userProfileId", source = "userProfile.userProfileId")
+    @Mapping(target = "username", source = "userProfile.username")
+    @Mapping(target = "fullname", source = "userProfile.fullname")
+    @Mapping(target = "email", source = "userProfile.email")
+    @Mapping(target = "phone", source = "userProfile.phone")
+    @Mapping(target = "address", source = "userProfile.address")
+    @Mapping(target = "dob", source = "userProfile.dob")
+    @Mapping(target = "createdAt", source = "userProfile.createdAt")
     @Mapping(target = "password", source = "password")
     ShopEmployeeRegistrationResponse toEmployeeResponse(UserProfile userProfile, EmployeeResponse employeeResponse, String password);
 }
