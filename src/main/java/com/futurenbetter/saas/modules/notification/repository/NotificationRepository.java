97c175551f93d1a8ccc951317c9034cfc0160bd6
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2026-02-22 11:15:15 +0700

    feat(notification): add config

--- src/main/java/com/futurenbetter/saas/modules/notification/repository/NotificationRepository.java
+++ src/main/java/com/futurenbetter/saas/modules/notification/repository/NotificationRepository.java
@@ -1,7 +1,7 @@
-//package com.futurenbetter.saas.modules.notification.repository;
-//
-//import com.futurenbetter.saas.modules.notification.entity.Notification;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface NotificationRepository extends JpaRepository<Notification, Long> {
-//}
+package com.futurenbetter.saas.modules.notification.repository;
+
+import com.futurenbetter.saas.modules.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+}
--- src/main/java/com/futurenbetter/saas/modules/notification/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/futurenbetter/saas/modules/notification/service/impl/NotificationServiceImpl.java
@@ -1,40 +1,40 @@
-//package com.futurenbetter.saas.modules.notification.service.impl;
-//
-//import com.futurenbetter.saas.modules.notification.entity.Notification;
-//import com.futurenbetter.saas.modules.notification.repository.NotificationRepository;
-//import com.futurenbetter.saas.modules.notification.service.inter.NotificationService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.messaging.simp.SimpMessagingTemplate;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class NotificationServiceImpl implements NotificationService {
-//
-//    private final NotificationRepository notificationRepository;
-//    private final SimpMessagingTemplate messagingTemplate;
-//
-//    @Transactional
-//    public void sendToUser(Notification notification) {
-//        Notification savedNotification = notificationRepository.save(notification);
-//
-//        // queue/notifications/{recipientType}/{recipientId}
-//        String destination = String.format("/queue/notifications/%s/%d",
-//                savedNotification.getRecipientType(),
-//                savedNotification.getRecipientId());
-//
-//        messagingTemplate.convertAndSend(destination, savedNotification);
-//    }
-//
-//    @Transactional
-//    public void sendToShopRole(Notification notification, String roleName) {
-//        Notification savedNotification = notificationRepository.save(notification);
-//
-//        // topic/shop/1/role/SHOP_MANAGER
-//        String destination = String.format("/topic/shop/%d/role/%s",
-//                savedNotification.getShop().getId(), roleName);
-//
-//        messagingTemplate.convertAndSend(destination, savedNotification);
-//    }
-//}
+package com.futurenbetter.saas.modules.notification.service.impl;
+
+import com.futurenbetter.saas.modules.notification.entity.Notification;
+import com.futurenbetter.saas.modules.notification.repository.NotificationRepository;
+import com.futurenbetter.saas.modules.notification.service.inter.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationServiceImpl implements NotificationService {
+
+    private final NotificationRepository notificationRepository;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @Transactional
+    public void sendToUser(Notification notification) {
+        Notification savedNotification = notificationRepository.save(notification);
+
+        // queue/notifications/{recipientType}/{recipientId}
+        String destination = String.format("/queue/notifications/%s/%d",
+                savedNotification.getRecipientType(),
+                savedNotification.getRecipientId());
+
+        messagingTemplate.convertAndSend(destination, savedNotification);
+    }
+
+    @Transactional
+    public void sendToShopRole(Notification notification, String roleName) {
+        Notification savedNotification = notificationRepository.save(notification);
+
+        // topic/shop/1/role/SHOP_MANAGER
+        String destination = String.format("/topic/shop/%d/role/%s",
+                savedNotification.getShop().getId(), roleName);
+
+        messagingTemplate.convertAndSend(destination, savedNotification);
+    }
+}
