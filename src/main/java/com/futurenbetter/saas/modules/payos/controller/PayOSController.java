382d930745ab335897ec567b17fd30c8e81cb63d
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2026-03-25 22:06:59 +0700

    feat(payos): using payos for payment

---
+++ src/main/java/com/futurenbetter/saas/modules/payos/controller/PayOSController.java
@@ -0,0 +1,35 @@
+package com.futurenbetter.saas.modules.payos.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.futurenbetter.saas.common.dto.response.ApiResponse;
+import com.futurenbetter.saas.modules.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vn.payos.PayOS;
+import vn.payos.model.webhooks.WebhookData;
+
+@RestController
+@RequestMapping("/payment")
+@RequiredArgsConstructor
+public class PayOSController {
+    private final PayOS payOS;
+    private final OrderService orderService;
+
+    // WEBHOOK
+    @PostMapping(path = "/payos_transfer_handler")
+    public ApiResponse<WebhookData> payosTransferHandler(@RequestBody Object body)
+            throws JsonProcessingException, IllegalArgumentException {
+        try {
+            WebhookData data = payOS.webhooks().verify(body);
+            orderService.updateOrderStatus(data);
+            return ApiResponse.success(HttpStatus.OK, "Xác thực webhook thành công", data, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ApiResponse.error(HttpStatus.BAD_REQUEST, "Xác thực webhook thất bại: " + e.getMessage(), null);
+        }
+    }
+}
