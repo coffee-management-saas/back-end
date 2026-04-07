6fe250b5d3315cd358f4f040383f2e97d002a805
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2026-04-08 02:11:49 +0700

    fix(cate): make categories api public

--- src/main/java/com/futurenbetter/saas/config/SecurityConfig.java
+++ src/main/java/com/futurenbetter/saas/config/SecurityConfig.java
@@ -27,7 +27,8 @@ public class SecurityConfig {
             "/swagger-ui/**",
             "/ws/**",
             "/payment/**",
-            "/api/system/auth/**"
+            "/api/system/auth/**",
+            "/api/categories/**",
     };
     private final AuthenticationService authenticationService;
         private static final String[] PUBLIC_ENDPOINTS = strings;
